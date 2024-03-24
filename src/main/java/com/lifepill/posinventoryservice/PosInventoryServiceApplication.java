7acda3824c5003afeac374287955b511b4404896
Pramitha Jayasooriya <lpramithamj@gmail.com>
2024-03-24 12:15:54 +0530

    Initialize inventory service

---
+++ src/main/java/com/lifepill/posinventoryservice/PosInventoryServiceApplication.java
@@ -0,0 +1,13 @@
+package com.lifepill.posinventoryservice;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class PosInventoryServiceApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(PosInventoryServiceApplication.class, args);
+	}
+
+}
